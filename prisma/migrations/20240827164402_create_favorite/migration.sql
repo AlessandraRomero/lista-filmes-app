3e4f0696fbd757a0dcd62eb5ba0bd6d40b5ca90b
AlessandraRomero <alessandraromeiro098@gmil.com>
2024-08-28 14:49:18 -0400

    chore: config

---
+++ prisma/migrations/20240827164402_create_favorite/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Favorite" (
+    "id" SERIAL NOT NULL,
+    "movieId" TEXT NOT NULL,
+
+    CONSTRAINT "Favorite_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Favorite_movieId_key" ON "Favorite"("movieId");
